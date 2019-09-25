53d96fc52619947a7abdfe710efc16fd2ce4ef15
Colin Braley <braley.colin@gmail.com>
2019-09-24 21:14:48 -0700

    Pass c++ version info via --cxxopt=-std=SOME_VERSION instead of "hard-coding" c++ version in the bazel rules.

--- scripts/build_and_run_benchmarks.sh
+++ scripts/build_and_run_benchmarks.sh
@@ -2,8 +2,28 @@
 
 set -e
 
+# Parse C++ version flag.
+CPP_VERSION="c++17"
+if [ "$#" -gt 1 ]; then
+  echo "Invalid arguments!!!!"
+  echo "Expected usage:"
+  echo "  ${0} [cpp_version]"
+  echo "If you specify a value for [cpp_version], we will pass this string to "
+  echo "the compiler as -std=[cpp_version]. If you don't specify a value, this "
+  echo "will default to c++17. Consider using \"c++11\", \"c++17\", or "
+  echo "\"c++14\"."
+ 	exit 1 
+elif [ "$#" -eq 1 ]; then
+  CPP_VERSION=${1}
+  echo "Using C++ version \"${CPP_VERSION}\""
+else
+  echo "Using default C++ version \"${CPP_VERSION}\""
+fi
+
+set -x
+
 # Build unit tests.
-bazel build -c opt test/...
+bazel build -c opt --cxxopt="-std=${CPP_VERSION}" test/...
 
 # Run benchmarks
 ./bazel-bin/test/thread_pool_benchmarks  \
