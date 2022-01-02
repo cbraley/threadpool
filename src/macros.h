0faeee0b035bc73e2b270aa5fc2848ae0d89d84f
Colin Braley <braley.colin@gmail.com>
2022-01-02 00:53:25 -0500

    Old tweaks I never committed.

--- src/macros.h
+++ src/macros.h
@@ -7,10 +7,10 @@
 // We want to use std::invoke if C++17 is available, and fallback to "hand
 // crafted" code if std::invoke isn't available.
 #if __cplusplus >= 201703L
-	#define INVOKE_MACRO(CALLABLE, ARGS_TYPE, ARGS)  std::invoke(CALLABLE, std::forward<ARGS_TYPE>(ARGS)...)
+  #define INVOKE_MACRO(CALLABLE, ARGS_TYPE, ARGS)  std::invoke(CALLABLE, std::forward<ARGS_TYPE>(ARGS)...)
 #elif __cplusplus >= 201103L
   // Update this with http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n4169.html.
-	#define INVOKE_MACRO(CALLABLE, ARGS_TYPE, ARGS)  CALLABLE(std::forward<ARGS_TYPE>(ARGS)...)
+  #define INVOKE_MACRO(CALLABLE, ARGS_TYPE, ARGS)  CALLABLE(std::forward<ARGS_TYPE>(ARGS)...)
 #else
   #error ("C++ version is too old! C++98 is not supported.")
 #endif
